cab6e2abb7f05dd69387a6ac872bbc6eab43efa4
Eugene Lysov <evg.lysov@gmail.com>
2020-10-21 15:25:45 +0300

    3

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
   entry: './src/index.js',
   output: {
       path: path.resolve(__dirname, 'build'),
       filename: 'my-bandle.js'
     },
-    module: {
-        rules: [{}]
-    },
-    devServer: {
-        port: 4444
-    },
+    
+    
 };
 
